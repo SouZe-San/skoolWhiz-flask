f43ee95b1179df5f4c2019b1fd8c5a132a3d0a3f
SouZe-San <103335953+SouZe-San@users.noreply.github.com>
2025-02-21 16:44:48 +0530

    readme Add 🦖

--- app.py
+++ app.py
@@ -3,40 +3,30 @@ from flask_restx import Api
 from src.routes.routes import api as todo_api
 from src.database import db_initialize
 import logging
-from flasgger import Swagger
 
 app = Flask(__name__)
-# api = Api(app)
-
-# Swagger(app, template={
-#     "info": {
-#         "title": "My Flask API",
-#         "description": "An example API using Flask and Swagger",
-#         "version": "1.0.0"
-#     }
-# })
 
 # Initialize database from schema.sql
 db_initialize()
 
 
-# @app.route('/', methods=['GET'])
-# def home():
-#     """
-#         This is an Test Url
-#         ---
-#         responses:
-#             200:
-#                 description: A successful response
-#                 examples:
-#                     application/text: "Welcome 🚀"
-#     """
-#     return "Welcome 🚀", 200
+@app.route('/', methods=['GET'])
+def home():
+    """
+        This is an Test Url
+        ---
+        responses:
+            200:
+                description: A successful response
+                examples:
+                    application/text: "Welcome 🚀"
+    """
+    return "Welcome 🚀,  cccls", 200
 
 
 # Swagger API Documentation
 api = Api(app, title="Todo API",
-          description="A simple Todo API using Flask", version="1.0")
+          description="A simple Todo API using Flask", version="1.0", doc="/api/docs")
 
 # Register API namespace
 api.add_namespace(todo_api, path="/todos")
